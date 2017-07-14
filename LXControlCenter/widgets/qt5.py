17c0151ba2cc95ab2c6afd81baa539ef4f3a4b70
Julien Lavergne <julien.lavergne@gmail.com>
2017-07-14 22:15:07 +0200

    qt5: Use default logic for icon handling, it still requiere Qt 5.9 for gtk cache

--- LXControlCenter/widgets/qt5.py
+++ LXControlCenter/widgets/qt5.py
@@ -44,11 +44,30 @@ class Qt5App(UI):
 
         #Function to launch at startup
         self.generate_view()
-        self.build_UI()
+        self.draw_ui()
 
-    def build_UI(self):
+    def clean_main_view(self):
+        for children in self.grid.children():
+            self.grid.removeItem(children)
+
+    def draw_ui(self):
+        if (self.mode == "main-UI"):
+            self.build_icon_view()
+
+    def build_icon_view(self):
+        self.clean_main_view()
+        # Generate the view again, to take the modifications of edit_view
+        self.generate_view()
+        self.build_generic_icon_view("visible")
+
+    def build_generic_icon_view(self, type_view):
+        items_to_draw = self.items_visible_by_categories
+
+        if (type_view == "all"):
+            items_to_draw = self.items_by_categories
+
         row = 0
-        for category in self.items_visible_by_categories:
+        for category in items_to_draw:
             groupBox = QGroupBox(category)
             groupGrid = QGridLayout()
             groupBox.setLayout(groupGrid)
@@ -56,21 +75,12 @@ class Qt5App(UI):
             row = row + 1
             groupCol = 0
             groupRow = 0
-            for i in self.items_visible_by_categories[category]:
-                if (len(i.icon) > 0):
-                    if (self.icon_not_theme_allow == True):
-                        if (i.icon[0] == "/"):
-                            #Absolute path
-                            try:
-                                pixbuf = QIcon(i.icon)
-                            except:
-                                pixbuf = QIcon.fromTheme(self.icon_fallback)
-                                logging.info("Error loading icon %s" % i.icon)                   
-                    else:
-                        try:
-                            pixbuf = QIcon.fromTheme(i.icon)
-                        except:
-                            pixbuf = QIcon.fromTheme(self.icon_fallback)
+# Need Qt 5.7 https://woboq.com/blog/qicon-reads-gtk-icon-cache-in-qt57.html
+            for i in items_to_draw[category]:
+                if (i.icon_type == "fix"):
+                    pixbuf = QIcon(i.icon)
+                elif (i.icon_type == "themed"):
+                    pixbuf = QIcon.fromTheme(i.icon, QIcon.fromTheme(self.icon_fallback))
                 else:
                     pixbuf = QIcon.fromTheme(self.icon_fallback)
 
@@ -78,12 +88,12 @@ class Qt5App(UI):
                 pixmap = QPixmap(pixbuf.pixmap(QSize(self.icon_view_icons_size, self.icon_view_icons_size)))
                 image = QLabel()
                 image.setPixmap(pixmap)
-                image.setAlignment(Qt.AlignCenter)
+                image.setAlignment(Qt.AlignLeft)
 
                 text = QLabel()
                 text.setText(i.name)
                 text.setWordWrap(True)
-                text.setAlignment(Qt.AlignCenter)
+                text.setAlignment(Qt.AlignLeft)
 
                 if (groupCol > self.icon_view_columns):
                     groupRow = groupRow + 1
