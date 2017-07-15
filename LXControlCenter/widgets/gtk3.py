8ad46f09203bd53dff8c83d0ed9169e1dbcaabb1
Julien Lavergne <julien.lavergne@gmail.com>
2017-07-15 16:50:22 +0200

    GTK3: Backport changes from GTK2 frontend, to make the 2 frontend in sync

--- LXControlCenter/widgets/gtk3.py
+++ LXControlCenter/widgets/gtk3.py
@@ -84,6 +84,10 @@ class Gtk3App(UI):
             self.action_group.get_action("PrefMode").set_visible(True)
             self.action_group.get_action("EditMode").set_visible(True)
             self.build_module_view()
+        elif (self.mode == "edit-item-UI"):
+            self.action_group.get_action("IconsMode").set_visible(True)
+            self.action_group.get_action("PrefMode").set_visible(True)
+            self.action_group.get_action("EditMode").set_visible(True)
         self.window.show_all()
 
     def build_toolbar(self):
@@ -116,6 +120,10 @@ class Gtk3App(UI):
         self.menu_button.add(image)
         self.header_bar.pack_start(self.menu_button)
 
+    def clean_main_view(self):
+        for children in self.content_ui_vbox.get_children():
+            self.content_ui_vbox.remove(children)
+
     def create_switch_conf(self, grid, label, default, group, key, position):
         label_widget = Gtk.Label(label)
         switch_widget = Gtk.Switch()
@@ -123,14 +131,6 @@ class Gtk3App(UI):
         switch_widget.connect("notify::active", self.on_switch_click, group, key)
         grid.attach(label_widget, 0, position, 1, 1)
         grid.attach_next_to(switch_widget, label_widget, Gtk.PositionType.RIGHT, 1, 1)
-
-    def create_table_conf(self):
-        grid = Gtk.Grid()
-        grid.set_column_homogeneous(False)
-        grid.set_row_homogeneous(False)
-        grid.set_column_spacing(20)
-        grid.set_row_spacing(20)
-        return grid
 
     def on_switch_click(self, switch, gparam, group, key):
         logging.debug("on_switch_click: Setting %s - %s to %s" % (group, key, switch.get_active()))
@@ -140,12 +140,30 @@ class Gtk3App(UI):
         self.triage_items()
         self.generate_view()
 
-    def clean_main_view(self):
-        for children in self.content_ui_vbox.get_children():
-            self.content_ui_vbox.remove(children)
+    # SpinButton
+    def create_spinbutton_conf(self, grid, label, default, group, key, position):
+        label_widget = Gtk.Label(label)
+        adjustment = Gtk.Adjustment(value=default, lower=8, upper=128, step_incr=1, page_incr=1, page_size=0)
+        spin_button_widget = Gtk.SpinButton(adjustment=adjustment, climb_rate=0.0, digits=0)
+        spin_button_widget.connect("value-changed", self.on_spinbutton_change, group, key)
+        grid.attach(label_widget, 0, position, 1, 1)
+        grid.attach_next_to(spin_button_widget, label_widget, Gtk.PositionType.RIGHT, 1, 1)
+
+    def on_spinbutton_change(self, spinbutton, group, key):
+        logging.debug("on_spinbutton_change: Setting %s - %s to %s" % (group, key, spinbutton.get_value_as_int()))
+        self.set_setting(group, key, spinbutton.get_value_as_int())
+        self.generate_view()
+
+    def create_table_conf(self):
+        grid = Gtk.Grid()
+        grid.set_column_homogeneous(False)
+        grid.set_row_homogeneous(False)
+        grid.set_column_spacing(20)
+        grid.set_row_spacing(20)
+        return grid
 
     def build_pref_view(self):
-        #TODO
+        #TODO Complete options
         self.clean_main_view()
     
         # Configuration
@@ -156,16 +174,28 @@ class Gtk3App(UI):
 
         self.create_switch_conf(configuration_grid, self.pref_modules_support_label, self.modules_support, "Configuration", "modules_support", 0)
         self.create_switch_conf(configuration_grid, self.pref_applications_support_label, self.applications_support, "Configuration", "applications_support", 1)
+        self.create_switch_conf(configuration_grid, self.pref_show_category_other_label, self.show_category_other, "Configuration", "show_category_other", 2)
+        self.create_spinbutton_conf(configuration_grid, self.pref_icon_view_icons_size, self.icon_view_icons_size, "Configuration", "icon_view_icons_size", 3)
 
     def build_edit_view(self):
-        #TODO
-        self.clean_main_view()       
+        self.clean_main_view()
+        self.build_generic_icon_view("all")
 
     def build_icon_view(self):
-
         self.clean_main_view()
+        # Generate the view again, to take the modifications of edit_view
+        self.generate_view()
+        self.build_generic_icon_view("visible")       
 
-        for category in self.items_visible_by_categories:
+    def build_generic_icon_view(self, type_view):
+
+        items_to_draw = self.items_visible_by_categories
+
+        if (type_view == "all"):
+            items_to_draw = self.items_by_categories
+
+        for category in items_to_draw:
+            logging.debug("build_generic_icon_view - category loop: %s" % category)
             frame = Gtk.Frame(label=category)
             self.content_ui_vbox.add(frame)
 
@@ -182,7 +212,10 @@ class Gtk3App(UI):
             iconview.set_selection_mode(Gtk.SelectionMode.SINGLE)
             iconview.set_activate_on_single_click(True)
 
-            iconview.connect("item-activated", self.on_item_activated)
+            if (self.mode == "main-UI"):
+                iconview.connect("item-activated", self.on_item_activated)
+            elif (self.mode == "edit-UI"):
+                iconview.connect("item-activated", self.on_item_edit_activated)
 
             logging.debug("build_UI: get_item_width = %s" % iconview.get_item_width())
             logging.debug("build_UI: get_spacing = %s" % iconview.get_spacing())
@@ -200,15 +233,27 @@ class Gtk3App(UI):
 
             self.define_icon_type_with_gtk_theme()
 
-            for i in self.items_visible_by_categories[category]:
+            for i in items_to_draw[category]:
+                logging.debug("build_generic_icon_view - item loop: %s" % i.path)
                 if (i.icon_type == "fix"):
                     pixbuf = GdkPixbuf.Pixbuf.new_from_file(i.icon)
                 elif (i.icon_type == "themed"):
-                    pixbuf = self.theme.load_icon(i.icon, self.icon_view_icons_size, icon_lookup_flags)
+                    try:
+                        pixbuf = self.theme.load_icon(i.icon, self.icon_view_icons_size, icon_lookup_flags)
+                    except:
+                        pixbuf = self.theme.load_icon(self.icon_fallback, self.icon_view_icons_size, icon_lookup_flags)
                 else:
                     pixbuf = self.theme.load_icon(self.icon_fallback, self.icon_view_icons_size, icon_lookup_flags)
+                logging.debug("build_generic_icon_view - item add: %s - %s" % (i.name, i.path))
 
-                liststore.append([pixbuf, i.name, i.path])
+                display_name = i.name
+
+                if (i.activate == False):
+                    logging.debug("build_generic_icon_view - Grey inactive icons: %s - %s" % (i.name, i.path))
+                    pixbuf.saturate_and_pixelate(pixbuf,0.1, True)
+                    display_name = _("(Inactive) - ") + i.name
+
+                liststore.append([pixbuf, display_name, i.path])
 
             hbox.add(iconview)
 
@@ -229,6 +274,115 @@ class Gtk3App(UI):
         logging.debug("on_item_activated: path = %s" % path)
         self.on_item_activated_common(path)
         icon_view.unselect_all()
+
+    def on_item_edit_activated(self, icon_view, tree_path):
+        logging.debug("on_item_edit_activated: click activated")
+        model = icon_view.get_model()
+        path = model[tree_path][2]
+        logging.debug("on_item_edit_activated: path = %s" % path)
+        self.build_edit_item_view(path)
+        icon_view.unselect_all()
+
+    def build_edit_item_view(self, path):
+        # TODO Icon image for icon
+        self.clean_main_view()
+        self.mode = "edit-item-UI"
+        self.item_to_save = self.items[path]
+
+        box_name = Gtk.HBox()
+        label_name = Gtk.Label()
+        label_name.set_text(_("Name"))
+        box_name.pack_start(label_name, False, True, 3)
+        self.entry_name = Gtk.Entry()
+        self.entry_name.set_text(self.item_to_save.name)
+        box_name.pack_start(self.entry_name, True, True, 3)
+        self.content_ui_vbox.pack_start(box_name, False, True, 3)
+
+        box_comment = Gtk.HBox()
+        label_comment = Gtk.Label()
+        label_comment.set_text(_("Comment"))
+        box_comment.pack_start(label_comment, False, True, 3)
+        self.entry_comment = Gtk.Entry()
+        self.entry_comment.set_text(self.item_to_save.comment)
+        box_comment.pack_start(self.entry_comment, True, True, 3)
+        self.content_ui_vbox.pack_start(box_comment, False, True, 3)
+
+        box_activate = Gtk.HBox()
+        self.check_activate = Gtk.CheckButton()
+        self.check_activate.set_label(_("Activate ?"))
+        self.check_activate.set_active(self.item_to_save.activate)
+        box_activate.add(self.check_activate)
+        self.content_ui_vbox.pack_start(box_activate, False, True, 0)
+
+        if (self.item_to_save.deactivate_reasons != []):
+            box_deactivate_reasons = Gtk.HBox()
+            deactivate_reasons_title = Gtk.Label()
+            deactivate_reasons_title.set_text(_("Reason(s) for deactivation"))
+            box_deactivate_reasons.add(deactivate_reasons_title)
+
+            deactivate_reasons_label = Gtk.Label()
+            label_to_display = ""
+            for label in self.item_to_save.deactivate_reasons:
+                label_to_display = label_to_display + label + "\n"
+            deactivate_reasons_label.set_text(label_to_display)
+            box_deactivate_reasons.add(deactivate_reasons_label)
+            self.content_ui_vbox.pack_start(box_deactivate_reasons, False, True, 3)
+
+        box_buttons = Gtk.HBox()
+        save_button = Gtk.Button()
+        save_button.set_label(_("Save"))
+        save_button.connect("clicked", self.on_edit_item_save)
+        box_buttons.add(save_button)
+        default_button = Gtk.Button()
+        default_button.set_label(_("Set to default values"))
+        default_button.connect("clicked", self.on_edit_item_default)
+        box_buttons.add(default_button)
+        cancel_button = Gtk.Button()
+        cancel_button.set_label(_("Cancel"))
+        cancel_button.connect("clicked", self.on_edit_item_cancel)
+        box_buttons.add(cancel_button)
+        self.content_ui_vbox.pack_start(box_buttons, False, True, 0)
+
+        # TODO Display other information about the desktop file
+        box_location = Gtk.HBox()
+        label_location = Gtk.Label()
+        label_location.set_text(_("Location"))
+        box_location.pack_start(label_location, False, True, 3)
+        self.entry_location = Gtk.Label()
+        self.entry_location.set_text(self.item_to_save.path)
+        box_location.pack_start(self.entry_location, True, True, 3)
+        self.content_ui_vbox.pack_start(box_location, False, True, 3)
+
+        self.draw_ui()
+
+    def on_edit_item_save(self, button):
+        if (self.item_to_save.name != self.entry_name.get_text()):
+            self.item_to_save.name = self.entry_name.get_text()
+            self.item_to_save.changed = True
+
+        if (self.item_to_save.comment != self.entry_comment.get_text()):
+            self.item_to_save.comment = self.entry_comment.get_text()
+            self.item_to_save.changed = True
+
+        if (self.item_to_save.activate != self.check_activate.get_active()):
+            self.item_to_save.activate = self.check_activate.get_active()
+            self.item_to_save.changed = True
+
+        self.mode = "edit-UI"
+        self.build_edit_view()
+        self.draw_ui()
+
+    def on_edit_item_cancel(self, button):
+        self.mode = "edit-UI"
+        self.build_edit_view()
+        self.draw_ui()
+
+    def on_edit_item_default(self, button):
+        self.entry_name.set_text(self.item_to_save.name_original)
+        #self.item_to_save.icon = self.item_to_save.icon_original
+        self.entry_comment.set_text(self.item_to_save.comment_original)
+        self.check_activate.set_active(self.item_to_save.activate_original)
+        self.item_to_save.changed = True
 
     def on_resize(self, widget, data=None):
         self.on_resize_common(self.window.get_size()[0], self.window.get_size()[1])
